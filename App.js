41c19bf2b64af9a68e864df75e133ca89c6f0c73
kingassociates426-ctrl <kingassociates426@gmail.com>
2025-08-08 00:32:34 +0400

    Update App.js

--- App.js
+++ App.js
@@ -31,3 +31,24 @@ export default function App() {
     </NavigationContainer>
   );
 }
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import LoginScreen from './LoginScreen';
+import ProfileScreen from './ProfileScreen';
+import VideoListScreen from './VideoListScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Login">
+        <Tab.Screen name="Login" component={LoginScreen} />
+        <Tab.Screen name="Videos" component={VideoListScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
